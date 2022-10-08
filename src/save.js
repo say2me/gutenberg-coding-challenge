5177a548b7970802672894ca69fc5bc9deea339b
Slava Vasenin <time2type@gmail.com>
2022-10-08 13:24:30 +0300

    Fix issues

--- src/save.js
+++ src/save.js
@@ -1,9 +1,21 @@
+/**
+ * WordPress dependencies
+ */
+import { useBlockProps } from '@wordpress/block-editor';
+
+/**
+ * Internal dependencies
+ */
 import Preview from './preview';
 
 export default function Save( { attributes } ) {
+	const { countryCode, relatedPosts } = attributes;
 	return (
-		<div>
-			<Preview { ...attributes } />
+		<div { ...useBlockProps.save() }>
+			<Preview
+				countryCode={ countryCode }
+				relatedPosts={ relatedPosts }
+			/>
 		</div>
 	);
 }
--- src/utils.js
+++ src/utils.js
@@ -1,8 +1,17 @@
-export function getEmojiFlag( countryCode ) {
+export const getEmojiFlag = ( countryCode ) => {
 	return String.fromCodePoint(
 		...countryCode
 			.toUpperCase()
 			.split( '' )
 			.map( ( char ) => 127397 + char.charCodeAt() )
 	);
-}
+};
+
+export const strippedRenderedExcerpt = ( renderedExcerpt ) => {
+	if ( ! renderedExcerpt ) return '';
+	const document = new window.DOMParser().parseFromString(
+		renderedExcerpt,
+		'text/html'
+	);
+	return document.body.textContent || document.body.innerText || '';
+};
